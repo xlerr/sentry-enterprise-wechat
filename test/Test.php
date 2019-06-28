579ed277b144821a05b2217a42a52c1ce24247d6
xlerr <xlerr@qq.com>
2019-06-28 17:34:10 +0800

    test

--- test/Test.php
+++ test/Test.php
@@ -3,23 +3,13 @@
 namespace xlerr\sentry\ewechat\test;
 
 use PHPUnit\Framework\TestCase;
-use xlerr\sentry\ewechat\Target;
 
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 final class Test extends TestCase
 {
-    private $target;
-
-    protected function setUp()
-    {
-        $this->target = new Target([
-            'config' => [],
-        ]);
-    }
-
     public function testInit()
     {
-        $this->assertInstanceOf(Target::class, $this->target, 'alksjdf');
+        $this->assertCount(1, [1], 'count error');
     }
 }
